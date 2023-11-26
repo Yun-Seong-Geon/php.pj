263dd78227d87e7f74d839e93bfcbd00e59bb94a
YUN <canyun6676@gmail.com>
2023-11-26 23:18:12 +0900

    pf 추가 및 php 탑바 연결

---
+++ frontend/topbar/topbar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../topbar/TopBar.css">
+
+
+</head>
+
+<body>
+    <div class="Top_bar">
+        <!-- 탑바 내용 -->
+        <a href="클릭시 메인페이지로 이동">
+            <img class="logo" src="../img/pf .png" alt="none">
+        </a>
+        
+        <div class="TopBar_middle">
+            <a href="../pf/pf.php">PF</a>
+            <a href="../board/board.php">게시판</a>
+            <a href="../notice/notice.php">공지사항</a>
+            <a href="../ai/ai.php">AI</a>
+        </div>
+
+        <div class="TopBar_right">
+            <a href="../login/login.php">로그인</a>
+            <a href="../signup/signup.php">회원가입</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
